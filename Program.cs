8137c1439ae561e559b1d4a0983ece82e09994d9
Bayez77 <mrbayez88@gmail.com>
2025-02-19 21:42:26 -0600

    Campsites

--- Program.cs
+++ Program.cs
@@ -26,6 +26,8 @@ builder.Services.Configure<JsonOptions>(Options =>
 
 
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -38,6 +40,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Swagger UI at the root
+});
+
 app.MapGet("/api/campsites", (CreekRiverDBContext db) => 
 {
     return db.Campsites.ToList();
@@ -45,7 +53,40 @@ app.MapGet("/api/campsites", (CreekRiverDBContext db) =>
 
 app.MapGet("/api/campsites/{id}", (CreekRiverDBContext db, int id) => 
 {
-    return db.Campsites.Include(c => c.CampsiteTypeId).Single(c => c.Id == id);
+    return db.Campsites.Include(c => c.CampsiteType).Single(c => c.Id == id);
 });
 
+app.MapPost("/api/campsites", (CreekRiverDBContext db, Campsite campsite) => 
+{
+    db.Campsites.Add(campsite);
+    db.SaveChanges();
+    return Results.Created($"/api/campsites/{campsite.Id}", campsite);
+});
+
+app.MapDelete("/api/campsites/{id}", (CreekRiverDBContext db, int id) => 
+{
+    Campsite campsite = db.Campsites.SingleOrDefault(campsite => campsite.Id == id);
+    if (campsite == null)
+    {
+        return Results.NotFound();
+    }
+    db.Campsites.Remove(campsite);
+    db.SaveChanges();
+    return Results.NoContent();
+});
+
+app.MapPut("/api/campsites/{id}", (CreekRiverDBContext db, int id, Campsite campsite) => 
+{
+    Campsite campsiteToUpdate = db.Campsites.SingleOrDefault(campsite => campsite.Id == id);
+    if (campsiteToUpdate == null)
+    {
+        return Results.NotFound();
+    }
+    campsiteToUpdate.NickName = campsite.NickName;
+    campsiteToUpdate.CampsiteTypeId = campsite.CampsiteTypeId;
+    campsiteToUpdate.ImageUrl = campsite.ImageUrl;
+
+    db.SaveChanges();
+    return Results.NoContent();
+});
 app.Run();
